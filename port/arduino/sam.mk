69ec537d2fbb8664ed32c02785d9c00ceac4247c
Tokita Hiroshi <tokita.hiroshi@gmail.com>
2020-09-13 06:28:53 -0700

    Define -DARDUINO_{build.board} macro
    Signed-off-by: Tokita Hiroshi <tokita.hiroshi@gmail.com>

--- port/arduino/sam.mk
+++ port/arduino/sam.mk
@@ -2,17 +2,8 @@
 ### It must be set to the board you are currently using. (i.e uno, mega2560, etc.)
 BOARD_TAG   = arduino_due_x
 
-### Variant Frequency
-F_CPU		= 84000000L
-### Board Chip
-MCU?=cortex-m3
-
 ### MCU ARCH
 ARCHITECTURE = sam
-
-#MCU VAriant
-VARIANT	= arduino_due_x
-
 
 ### MONITOR_PORT
 ### The port your board is connected to. Using an '*' tries all the ports and finds the right one.
@@ -21,7 +12,7 @@ MONITOR_PORT      = /dev/ttyACM*
 
 # Define Arduino support package installation path where SAM device support has been installed
 # Linux
-ARDUINO_PACKAGE_DIR := $(HOME)/.arduino15/packages
+#ARDUINO_PACKAGE_DIR := $(HOME)/.arduino15/packages
 
 
 include $(ARDMK_DIR)/Sam.mk
--- port/arduino/samd.mk
+++ port/arduino/samd.mk
@@ -2,16 +2,8 @@
 ### It must be set to the board you are currently using. (i.e uno, mega2560, etc.)
 BOARD_TAG   = mkrzero
 
-### Variant Frequency
-F_CPU		= 48000000L
-### Board Chip
-MCU?=cortex-m0plus
-
 ### MCU ARCH
 ARCHITECTURE=samd
-
-#MCU VAriant
-VARIANT			= mkrzero
 
 ### MONITOR_PORT
 ### The port your board is connected to. Using an '*' tries all the ports and finds the right one.
@@ -20,8 +12,7 @@ MONITOR_PORT    = /dev/ttyACM*
 
 # Define Arduino support package installation path where SAM device support has been installed
 # Linux
-ARDUINO_PACKAGE_DIR := $(HOME)/.arduino15/packages
-
+#ARDUINO_PACKAGE_DIR := $(HOME)/.arduino15/packages
 
 include $(ARDMK_DIR)/Sam.mk
 
